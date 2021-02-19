27d3bb9ab92c9fd85968d64d744e0da3612c4abc
Tumen Tumurchudur <tumen.tumurchudur@gmail.com>
2021-02-19 14:52:48 -0700

    Changes class component to functional component

--- src/App.js
+++ src/App.js
@@ -1,59 +1,60 @@
-import './App.css';
-import React, { Component } from "react";
+import "./App.css";
+import React, { useState } from "react";
 //import JobTask from './Components/JobTasksComp/JobTasks';
-import PayRollInfo from './Components/PayRollInfoComp/PayRollInfo';
-import ApproverComp from './Components/ApproverComp';
-import RadioGroup from './Components/Common/radio_group';
-import NewComp from './Components/NewComp';
-import WorkHours from './Components/HoursTab/DaysOfWeekHours';
-import FcoQpmsComp from './Components/FcoQmpsTab/FcoQmpsComp';
+import PayRollInfo from "./Components/PayRollInfoComp/PayRollInfo";
+import ApproverComp from "./Components/ApproverComp";
+import RadioGroup from "./Components/Common/radio_group";
+import NewComp from "./Components/NewComp";
+import WorkHours from "./Components/HoursTab/DaysOfWeekHours";
+import FcoQpmsComp from "./Components/FcoQmpsTab/FcoQmpsComp";
 //import CommentsComp from './Components/Common/TabsComponents/CommentsTab/CommentsComp';
-import CommentsComp from './Components/CommentsTab/CommentsComp';
-import ProjectComp from './Components/ProjectTab/ProjectComp';
-import TaskInfoComp from './Components/TaskInfoTab/TaskInfoComp';
+import CommentsComp from "./Components/CommentsTab/CommentsComp";
+import ProjectComp from "./Components/ProjectTab/ProjectComp";
+import TaskInfoComp from "./Components/TaskInfoTab/TaskInfoComp";
 
-class App extends Component {
-  // styles = { fontStyle: "bold", color: "teal" }
-  constructor(props) {
-    super(props);
-    this.state = {
-      display: "Hours"
-    }
-  }
+// Move this to another folder in which you keep track of enums or constants.
+const Tabs = {
+  Hours: "Hours",
+  FCOQPMS: "FCOQPMS",
+  ClientData: "ClientData",
+  Comments: "Comments",
+  TSCorr: "TSCorr",
+  Project: "Project",
+  TaskInfo: "TaskInfo",
+};
+function App() {
+  const [display, setDisplay] = useState("Hours");
 
-  view = (tag) => this.setState(state => ({ display: tag }));
-  getTab = () => {
-    switch (this.state.display) {
-      case "Hours":
-        return (<WorkHours />);
-      case "FCOQPMS":
-        return (<FcoQpmsComp />);
-      case "ClientData":
-        return (<label>WAITING FOR REQUIREMENTS.</label>);
-      case "Comments":
-        return (<CommentsComp />);
-      case "TSCorr":
-        return (<label>WAITING FOR REQUIREMENTS.</label>);
-      case "Project":
-        return (<ProjectComp />);
-      case "TaskInfo":
-        return (<TaskInfoComp />);
+  const getTab = () => {
+    switch (display) {
+      case Tabs.Hours:
+        return <WorkHours />;
+      case Tabs.FCOQPMS:
+        return <FcoQpmsComp />;
+      case Tabs.ClientData:
+        return <label>WAITING FOR REQUIREMENTS.</label>;
+      case Tabs.Comments:
+        return <CommentsComp />;
+      case Tabs.TSCorr:
+        return <label>WAITING FOR REQUIREMENTS.</label>;
+      case Tabs.Project:
+        return <ProjectComp />;
+      case Tabs.TaskInfo:
+        return <TaskInfoComp />;
       default:
-        return (<WorkHours />);
+        return <WorkHours />;
     }
-  }
+  };
 
-  render() {
-
-    console.log(this.state.display);
-    return (
-      <div className="App">
-        <NewComp/>
-        <div className="container">
-          <div><RadioGroup screen={this.view} /></div><div>{this.getTab()}</div>
-        </div>
+  return (
+    <div className="App">
+      <NewComp />
+      <div className="container">
+        <RadioGroup screen={(tag) => setDisplay(tag)} />
+        {getTab()}
       </div>
-    )
-  }
+    </div>
+  );
 }
+
 export default App;
