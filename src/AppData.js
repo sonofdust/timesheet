38b392e6fc9bfac908f74b44d02053cce866035e
nicrom06 <nicrom06@Protiviti.com>
2021-02-20 10:24:21 -0700

    Changes

--- src/AppData.js
+++ src/AppData.js
@@ -1,165 +1,110 @@
-
 export const JobTaskList = {
-    "1A": ["A001", "A002", "A003", "A004", "A005"],
-    "2B": ["B001", "B002", "B003", "B004", "B005"],
-    "3C": ["C001", "C002", "C003", "C004", "C005"],
-    "4D": ["D001", "D002", "D003", "D004", "D005"],
-    "5E": ["E001", "E002", "E003", "E004", "E005"]
-}
+  "1A": ["A001", "A002", "A003", "A004", "A005"],
+  "2B": ["B001", "B002", "B003", "B004", "B005"],
+  "3C": ["C001", "C002", "C003", "C004", "C005"],
+  "4D": ["D001", "D002", "D003", "D004", "D005"],
+  "5E": ["E001", "E002", "E003", "E004", "E005"],
+};
 
-export const AppData =
-{
-    "userInfo": {
-        "employeeId": "2",
-        "username": "FUDD",
-        "fullName": "Fudd, Elmer Aloysious",
-        "role": "Exec Asst",
-        "phone": "808/555-1212",
-        "workLocation": "HOU",
-        "payrollId": "123456789",
-        "dept": "D001",
-        "ptoHrs": "0"
+export const AppData = {
+  userInfo: {
+    employeeId: "2",
+    username: "FUDD",
+    fullName: "Fudd, Elmer Aloysious",
+    role: "Exec Asst",
+    phone: "808/555-1212",
+    workLocation: "HOU",
+    payrollId: "123456789",
+    dept: "D001",
+    ptoHrs: "0",
+  },
+  weDate: "2021-02-20",
+  dateDesc: "Week 1 of 2 For Pay Cycle",
+  payrollCycleDate: "2021-02-27",
+  submittedDate: null,
+  approvedDate: null,
+  total: 800,
+  dateSigned: null,
+  approverMsg: null,
+  taskDetails: [
+    {
+      jobId: 2,
+      jobName: "AB001",
+      taskId: 2,
+      taskName: "T002",
+      time: {
+        reg: [0, 0, 800, 0, 0, 0, 0],
+        regTotal: 800,
+        ot: [0, 0, 0, 0, 0, 0, 0],
+        otTotal: 0,
+      },
+      comments: "This is a time sheet task comment",
+      project: {
+        projectId: "2",
+        jobNo: "AB001",
+        name: "Project Two",
+        clientId: "2",
+        clientName: "Eco Focus",
+        projectMgr: "Fudd, Elmer Aloysious",
+        costEng: "Samuels, Rhonda",
+        billing: "Whitson, Ben",
+        apContacts: "Samuels, Rhonda",
+      },
+      task: {
+        taskId: "2",
+        dateOpened: "04/01/2020",
+        name: "T002",
+        description: "Phase 3 Scoping",
+      },
+      client: {
+        clientId: "2",
+        name: "Eco Focus",
+        description: "Client Two",
+      },
+      fcoNum: "fco",
+      qpmsCode: "qpms",
+      workSite: "site",
     },
-    "weDate": "2021-02-20",
-    "dateDesc": "Week 1 of 2 For Pay Cycle",
-    "payrollCycleDate": "2021-02-27",
-    "submittedDate": null,
-    "approvedDate": null,
-    "total": 800,
-    "dateSigned": null,
-    "approverMsg": null,
-    "taskDetails": [
-        {
-            "jobId": 2,
-            "jobName": "AB001",
-            "taskId": 2,
-            "taskName": "T002",
-            "time": {
-                "reg": [
-                    0,
-                    0,
-                    800,
-                    0,
-                    0,
-                    0,
-                    0
-                ],
-                "regTotal": 800,
-                "ot": [
-                    0,
-                    0,
-                    0,
-                    0,
-                    0,
-                    0,
-                    0
-                ],
-                "otTotal": 0
-            },
-            "comments": "This is a time sheet task comment",
-            "project": {
-                "projectId": "2",
-                "jobNo": "AB001",
-                "name": "Project Two",
-                "clientId": "2",
-                "clientName": "Eco Focus",
-                "projectMgr": "Fudd, Elmer Aloysious",
-                "costEng": "Samuels, Rhonda",
-                "billing": "Whitson, Ben",
-                "apContacts": "Samuels, Rhonda"
-            },
-            "task": {
-                "taskId": "2",
-                "dateOpened": "04/01/2020",
-                "name": "T002",
-                "description": "Phase 3 Scoping"
-            },
-            "client": {
-                "clientId": "2",
-                "name": "Eco Focus",
-                "description": "Client Two"
-            },
-            "fcoNum": "fco",
-            "qpmsCode": "qpms",
-            "workSite": "site"
-        },
 
-
-        {
-            "jobId": 2,
-            "jobName": "DDT01",
-            "taskId": 2,
-            "taskName": "ZZ02",
-            "time": {
-                "reg": [
-                    0,
-                    0,
-                    800,
-                    800,
-                    800,
-                    800,
-                    0
-                ],
-                "regTotal": 800,
-                "ot": [
-                    0,
-                    100,
-                    200,
-                    300,
-                    100,
-                    0,
-                    0
-                ],
-                "otTotal": 0
-            },
-            "comments": "This is a time sheet task comment",
-            "project": {
-                "projectId": "2",
-                "jobNo": "AB001",
-                "name": "Project Two",
-                "clientId": "2",
-                "clientName": "Eco Focus",
-                "projectMgr": "Fudd, Elmer Aloysious",
-                "costEng": "Samuels, Rhonda",
-                "billing": "Whitson, Ben",
-                "apContacts": "Samuels, Rhonda"
-            },
-            "task": {
-                "taskId": "2",
-                "dateOpened": "04/01/2020",
-                "name": "T002",
-                "description": "Phase 3 Scoping"
-            },
-            "client": {
-                "clientId": "2",
-                "name": "Eco Focus",
-                "description": "Client Two"
-            },
-            "fcoNum": "fco",
-            "qpmsCode": "qpms",
-            "workSite": "site"
-        }
-
-
-
-
-    ],
-    "totalReg": [
-        0,
-        0,
-        800,
-        0,
-        0,
-        0,
-        0
-    ],
-    "totalOt": [
-        0,
-        0,
-        0,
-        0,
-        0,
-        0,
-        0
-    ]
-}
+    {
+      jobId: 2,
+      jobName: "DDT01",
+      taskId: 2,
+      taskName: "ZZ02",
+      time: {
+        reg: [0, 0, 800, 800, 800, 800, 0],
+        regTotal: 800,
+        ot: [0, 100, 200, 300, 100, 0, 0],
+        otTotal: 0,
+      },
+      comments: "This is a time sheet task comment",
+      project: {
+        projectId: "2",
+        jobNo: "AB001",
+        name: "Project Two",
+        clientId: "2",
+        clientName: "Eco Focus",
+        projectMgr: "Fudd, Elmer Aloysious",
+        costEng: "Samuels, Rhonda",
+        billing: "Whitson, Ben",
+        apContacts: "Samuels, Rhonda",
+      },
+      task: {
+        taskId: "2",
+        dateOpened: "04/01/2020",
+        name: "T002",
+        description: "Phase 3 Scoping",
+      },
+      client: {
+        clientId: "2",
+        name: "Eco Focus",
+        description: "Client Two",
+      },
+      fcoNum: "fco",
+      qpmsCode: "qpms",
+      workSite: "site",
+    },
+  ],
+  totalReg: [0, 0, 800, 0, 0, 0, 0],
+  totalOt: [0, 0, 0, 0, 0, 0, 0],
+};
--- src/Components/HoursTab/DaysOfWeekHours.js
+++ src/Components/HoursTab/DaysOfWeekHours.js
@@ -1,63 +1,150 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
-import { JobTaskList, AppData } from '../../AppData';
-import ItemSelect from '../Common/itemSelect';
-import { v4 } from 'uuid';
+// import React from 'react';
+// import Box from '@material-ui/core/Box';
 
-const useStyles = makeStyles({
-    table: {
-        minWidth: 650
-    }
-})
+// export default function Inline() {
+//   return (
+//     <div style={{ width: '100%' }}  border = {1} p={1} m={1}>
+//       <Box component="div" display="inline" border={1} bgcolor="background.defalut">
+//         inline
+//       </Box>
+//       <Box component="div" display="inline" border={1} bgcolor="background.default">
+//         inline
+//       </Box>
+//     </div>
+//   );
+// }
+
+// import React from 'react';
+// import { makeStyles } from '@material-ui/core/styles';
+// import Paper from '@material-ui/core/Paper';
+// import Grid from '@material-ui/core/Grid';
+
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//     flexGrow: 1,
+//   },
+//   paper: {
+//     padding: theme.spacing(2),
+//     textAlign: 'center',
+//     color: theme.palette.text.secondary,
+//   },
+// }));
+
+// export default function CenteredGrid() {
+//   const classes = useStyles();
+
+//   return (
+//     <div className={classes.root}>
+//       <Grid container spacing={1}>
+//         <Grid item xs={3}>
+//           <Paper className={classes.paper}>xs=1</Paper>
+//         </Grid>
+//         <Grid item xs={3}>
+//           <Paper className={classes.paper}>xs=6</Paper>
+//         </Grid>
+//         <Grid item xs={3}>
+//           <Paper className={classes.paper}>xs=6</Paper>
+//         </Grid>
+//         <Grid item xs={3}>
+//           <Paper className={classes.paper}>xs=3</Paper>
+//         </Grid>
+//         <Grid item xs={3}>
+//           <Paper className={classes.paper}>xs=3</Paper>
+//         </Grid>
+//         <Grid item xs={3}>
+//           <Paper className={classes.paper}>xs=3</Paper>
+//         </Grid>
+//         <Grid item xs={3}>
+//           <Paper className={classes.paper}>xs=3</Paper>
+//         </Grid>
+//       </Grid>
+//     </div>
+//   );
+// }
+
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import { JobTaskList, AppData } from "../../AppData";
+import ItemSelect from "../Common/itemSelect";
+import { v4 } from "uuid";
+
+// const useStyles = makeStyles({
+//     table: {
+//         minWidth: 650
+//     }
+// })
 
 const DAY = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
 
 const header = ["Job ID", "Task ID", "", ...DAY];
 
+const reg = (e) => (
+  <TableRow key={v4}>
+    <TableCell align="left">{e.jobName}</TableCell>
+    <TableCell align="left">{e.taskName}</TableCell>
+    <TableCell align="left">REG</TableCell>
+    {e.time.reg.map((t, i) => (
+      <TableCell align="left">{t}</TableCell>
+    ))}
+  </TableRow>
+);
+
+const ot = (e) => (
+  <TableRow key={v4}>
+    <TableCell align="left"></TableCell>
+    <TableCell align="left"></TableCell>
+    {<TableCell align="left">OT</TableCell>}
+    {e.time.ot.map((t, i) => (
+      <TableCell align="left">{t}</TableCell>
+    ))}
+  </TableRow>
+);
+
 export default function MyTable() {
-    const classes = useStyles();
-    return (
-        <div className="container">
-            {/* <pre>{JSON.stringify(AppData.taskDetails[0])}</pre> */}
-            <div className="container-row">
+  // const classes = useStyles();
+  return (
+    <div className="container">
+      {/* <pre>{JSON.stringify(AppData.taskDetails[0])}</pre> */}
+      <div className="container-row">
+        {/* <JobTasksComp /> */}
+        <TableContainer component={Paper}>
+          <Table aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                {header.map((e, i) => (
+                  <TableCell align="left">{e}</TableCell>
+                ))}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {AppData.taskDetails.map(
+                (e) =>
+                  // <TableRow key={v4}>
+                  //     <TableCell align="left">{e.jobName}</TableCell>
+                  //     <TableCell align="left">{e.taskName}</TableCell>
+                  //     <TableCell align="left">REG<br />OT</TableCell>
+                  //     {e.time.reg.map((t, i) => (<TableCell align="left">{t}<br />{e.time.ot[i]}</TableCell>))}
+                  // </TableRow>
 
-                {/* <JobTasksComp /> */}
-                <TableContainer component={Paper}>
-                    <Table className={classes.table} aria-label="simple table">
-                        <TableHead>
-                            <TableRow>
-                                {header.map((e, i) => (<TableCell align="left">{e}</TableCell>))}
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {AppData.taskDetails.map((e) =>
-                            (
-                                <TableRow key={v4}>
-                                    <TableCell align="left">{e.jobName}</TableCell>
-                                    <TableCell align="left">{e.taskName}</TableCell>
-                                    <TableCell align="left">REG<br/>OT</TableCell>
-                                    {e.time.reg.map((t, i) => (<TableCell align="left">{t}<br/>{e.time.ot[i]}</TableCell>))}
-                                </TableRow>
-
-                                // <TableRow key={v4}>
-                                //                                     <TableCell align="left"></TableCell>
-                                //                                     <TableCell align="left"></TableCell>
-                                //                                     {<TableCell align="left">{`OT`}</TableCell>}
-                                //                                     {e.time.ot.map((t, i) => (<TableCell align="left">{t}</TableCell>))}
-                                // </TableRow>
-
-                            ))}
-                        </TableBody>
-                    </Table>
-                </TableContainer>
-            </div>
-        </div>
-    )
+                  reg(e)
+                // <TableRow key={v4}>
+                //                                     <TableCell align="left"></TableCell>
+                //                                     <TableCell align="left"></TableCell>
+                //                                     {<TableCell align="left">{`OT`}</TableCell>}
+                //                                     {e.time.ot.map((t, i) => (<TableCell align="left">{t}</TableCell>))}
+                // </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
+    </div>
+  );
 }
